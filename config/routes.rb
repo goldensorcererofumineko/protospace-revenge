bd9725ea2ac5071a85e197b6d7225e76a53f16c6
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-09-21 16:56:16 +0900

    失敗

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: :index
+  resources :prototypes, only: [:index, :new, :create, :destroy,:show]
+  resources :users, only: [:edit, :update]
 end
